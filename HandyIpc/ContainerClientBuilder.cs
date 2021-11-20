656d244504ab8e560bbd56be82af0f03df6765a2
Dingping Zhang <DingpingZhang@outlook.com>
2021-11-20 21:43:05 +0800

    refactor: some trivial changes.

--- HandyIpc/ContainerClientBuilder.cs
+++ HandyIpc/ContainerClientBuilder.cs
@@ -12,7 +12,7 @@ namespace HandyIpc
         private Func<ISerializer> _serializerFactory = () => throw new InvalidOperationException(
             $"Must invoke the {nameof(IServerConfiguration)}.Use(Func<{nameof(ISerializer)}> factory) method " +
             "to register a factory before invoking the Build method.");
-        private Func<ILogger> _loggerFactory = () => new DefaultLogger();
+        private Func<ILogger> _loggerFactory = () => new TraceLogger();
 
         public IClientConfiguration Use(Func<ISerializer> factory)
         {
